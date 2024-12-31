ac98cccf4be628ae075bd7a943b149018eb1c7be
Rob Bradley <rob.bradley@brandstudio-external.corp-partner.google.com>
2024-12-31 14:51:49 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// app.js
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Configure Nunjucks
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Define routes
+app.get('/index', (req, res) => {
+  res.render('index.njk', { title: 'Home' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.njk', { title: 'About' });
+});
+
+app.get('/artwork', (req, res) => {
+  res.render('artwork.njk', { title: 'Artwork' });
+});
+
+app.get('/exhibitions', (req, res) => {
+  res.render('exhibitions.njk', { title: 'Exhibitions' });
+});
+
+app.get('/books', (req, res) => {
+  res.render('books.njk', { title: 'Books' });
+});
+
+app.get('/press', (req, res) => {
+  res.render('press.njk', { title: 'Press' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact.njk', { title: 'Contact' });
+});
+
+app.get('/subscribe', (req, res) => {
+  res.render('subscribe.njk', { title: 'Subscribe' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ html/readme.md
@@ -0,0 +1,12 @@
+- removing any old code from previous updates over the years we don't want
+- split up the codebase into modules or chunks, meaning updates in future will take less time moving ahead
+- use the opportunity to optimise pages for SEO (we didn't really do that before i just remembered)
+- customise meta tags for each individual page - at the moment all pages use same meta tags - not a problem but google does like precision and accuracy on a page by page basis. when their bots evaluate sites during indexing, all these things help rankings.
+
+also some amends
+
+- slight tweak to layout to subscribe page - see attached, i think this looks better.
+- remove "basic site" button option on splash page - we don't need this anymore, difference is negligible with last update. we only whacked it in on prev updates because of autoplay / audio / video browser restrictions.
+- update copyright to 2025
+
+let me know what you think, around the £250-£300 mark for all this
